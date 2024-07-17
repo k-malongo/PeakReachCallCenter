ad96cf0d521b01ff195ea70aacff1c1f242fbeae
Arnold Obiero <odhisoarnold@gmail.com>
2024-07-17 19:04:46 +0300

    try header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 export default function Header() {
 	return (
 		<div id="header">
-            <img className="background" src="/public/images/backgroundheader.png" alt="peakreach"/>
-			<div className="content">
+			<img
+				className="background"
+				src="/images/header2.png"
+				alt="header background"
+			/>
 
+			<div className="content">
 				<div className="card">
 					<h2 className="title">
 						Empowering Customer Experiences with Innovation and Sustainability
