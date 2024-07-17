6ee0df8db4e4641e2d7a58e453a2eec62254a850
Arnold Obiero <odhisoarnold@gmail.com>
2024-07-17 17:26:38 +0300

    add header description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,12 @@ export default function Header() {
 				Empowering Customer Experiences with Innovation and Sustainability in
 				Kenya's Premier Call Center
 			</h2>
+			<h3>
+				PeakReach is a Kenyan call center where customer satisfaction, advanced
+				technology, and sustainability converge. Our exceptional workforce
+				builds long-term relationships, delivering unparalleled service and
+				ensuring customer service excellence.
+			</h3>
 		</div>
 	);
 }
