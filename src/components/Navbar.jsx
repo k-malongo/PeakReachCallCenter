7021f33b1b9ad2375e53c29e15eebea8adf7dbd2
Arnold Obiero <odhisoarnold@gmail.com>
2024-07-18 14:10:14 +0300

    add services content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <div id="navbar">
-      <div className="content">
-        <div className="brand">
-          <img className="logo" src="/images/logo1.png" alt="" />
-          <h1>PeakReach</h1>
-        </div>
+	return (
+		<div id="navbar">
+			<div className="content">
+				<div className="brand">
+					<img className="logo" src="/images/logo1.png" alt="" />
+					<h1>Peak Reach</h1>
+				</div>
 
-        <div className="links">
-            <Link to="/">Home</Link>
-            <Link to="/">Services</Link>
-            <Link to="/">Products</Link>
-            <Link to="/">Contact</Link>
-            <Link to="/">About</Link>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="links">
+					<Link to="/">Home</Link>
+					<Link to="/">Services</Link>
+					<Link to="/">Products</Link>
+					<Link to="/">Contact</Link>
+					<Link to="/">About</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,47 @@
-export default function Services(){
-    return (
-        <div id="services">
-            <h2>services</h2>
-        </div>
-    )
+export default function Services() {
+	return (
+		<div id="services">
+			<h3>some of the services we offer</h3>
+			<h3>
+				PeakReach has extensive experience providing comprehensive Solutions for
+				Exceptional Customer Engagement and Business Growth
+			</h3>
+
+			<div className="card">
+				<h4>Inbound and Outbound Calls</h4>
+				<p>
+					Our team handles both inbound and outbound calls with efficiency and
+					professionalism, ensuring every customer interaction is positive and
+					productive.
+				</p>
+			</div>
+
+			<div className="card">
+				<h4>Debt Collection</h4>
+				<p>
+					We provide reliable debt collection services, using ethical and
+					effective methods to help recover outstanding debts while maintaining
+					strong customer relationships.
+				</p>
+			</div>
+
+			<div className="card">
+				<h4>Lead Generation</h4>
+				<p>
+					We specialize in lead generation, using advanced strategies to
+					identify and cultivate potential customers, driving business growth
+					and success.
+				</p>
+			</div>
+
+			<div className="card">
+				<h4>Live Chat Support</h4>
+				<p>
+					Our live chat support offers real-time assistance, ensuring customers
+					receive immediate help and resolutions, enhancing their overall
+					experience.
+				</p>
+			</div>
+		</div>
+	);
 }
