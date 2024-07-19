b28862dc4e7833d142a720630ebc952595a27dfb
Arnold Obiero <odhisoarnold@gmail.com>
2024-07-19 19:30:39 +0300

    add testimonials content

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -24,48 +24,56 @@ const testimonials = [
 	{
 		name: "John M.",
 		company: "Acme Inc.",
+		image: "boy1.jpg",
 		statement:
 			"PeakReach's customer service is top-notch. Their team is always professional and responsive, making our interactions smooth and efficient.",
 	},
 	{
 		name: "Susan K.",
 		company: "Bright Solutions",
+		image: "girl1.jpg",
 		statement:
 			"We've seen a significant improvement in our debt recovery rates since partnering with PeakReach. Their ethical approach and effectiveness are commendable.",
 	},
 	{
 		name: "Michael O.",
 		company: "Tech Innovators",
+		image: "boy2.jpg",
 		statement:
 			"The inbound and outbound call services provided by PeakReach have been a game changer for our business. Our customers are happier, and our operations are more streamlined.",
 	},
 	{
 		name: "Grace N.",
 		company: "Marketing Pros",
+		image: "girl2.jpg",
 		statement:
 			"PeakReach's lead generation services have helped us grow our customer base significantly. Their strategies are advanced and highly effective.",
 	},
 	{
 		name: "David W.",
 		company: "E-commerce Ventures",
+		image: "boy3.jpg",
 		statement:
 			"The live chat support from PeakReach has been exceptional. Our customers receive immediate assistance, which has greatly enhanced their satisfaction.",
 	},
 	{
 		name: "Linda A.",
 		company: "Retail Giants",
+		image: "girl3.jpg",
 		statement:
 			"Working with PeakReach has been a pleasure. Their dedication to building long-term relationships and providing excellent service is evident in every interaction.",
 	},
 	{
 		name: "Peter R.",
 		company: "Financial Solutions",
+		image: "boy4.jpg",
 		statement:
 			"Our customer service ratings have soared since we started using PeakReach's services. Their team is dedicated, professional, and always ready to help.",
 	},
 	{
 		name: "Mary T.",
 		company: "Health Services Inc.",
+		image: "girl4.jpg",
 		statement:
 			"PeakReach has truly exceeded our expectations. Their comprehensive solutions have improved our customer engagement and driven significant business growth.",
 	},
@@ -92,9 +100,19 @@ export default function Testimonials() {
 				{testimonials.map((testimonial, index) => {
 					return (
 						<div className="card" key={index}>
-							<h4>{testimonial.name}</h4>
-							<h4>{testimonial.company}</h4>
 							<p>{testimonial.statement}</p>
+							<div className="personal_info">
+								<div className="portrait">
+									<img
+										src={`/images/${testimonial.image}`}
+										alt={testimonial.name}
+									/>
+								</div>
+								<div>
+									<h4>{testimonial.name}</h4>
+									<h4>{testimonial.company}</h4>
+								</div>
+							</div>
 						</div>
 					);
 				})}
